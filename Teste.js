55ba7159e87856f93c613403ce913dbc5c6c0ba2
Murilosoder <03024603069@senacrs.edu.br>
2025-12-01 11:04:55 -0300

    Delete Teste.js

--- Teste.js
+++
@@ -1,8 +0,0 @@
-function adicionarTarefa() {
-  const input = document.getElementById("tarefaInput");
-  if (!input.value) return alert("Digite uma tarefa antes de adicionar!");
-  const li = document.createElement("li");
-  li.textContent = input.value;
-  document.getElementById("listaTarefas").appendChild(li);
-  input.value = "";
-}
